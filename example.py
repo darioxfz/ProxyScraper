6c61af1f32b73b1d27a3eb1ca1bd76098c155384
vorticalbox <vorticalbox@outlook.com>
2017-08-09 21:00:49 +0100

    Added new site support

--- example.py
+++ example.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-import vboxproxies as vb
+import proxyScraper as ps
 import requests
 import json
-session = requests.session()
-for i in range(5):
-    print("")
-proxies = vb.getProxies('http://google.co.uk',50)
-print("{0} working".format(len(proxies)))
-for p in proxies:
+from multiprocessing import Pool
+
+def getIP(p):
+    session = requests.session()
     with session as s:
         try:
-            r = s.get("http://ip-api.com/json", proxies={'https' :p, 'http':p}, timeout=1)
+            r = s.get("http://ip-api.com/json", proxies={'https' :p, 'http':p}, timeout=5)
             j = json.loads(r.text)
-            print(j['query'])
+            print('my IP is {0}'.format(j['query']))
         except:
             print('{0} Failed'.format(p))
+
+proxies = ps.getProxies('https://www.google.co.uk',50)
+print("{0} working".format(len(proxies)))
+with Pool(50) as p:
+    p.map(getIP, proxies)
--- proxyScraper/__init__.py
+++ proxyScraper/__init__.py
@@ -25,7 +25,7 @@ def getProxies(u,t):
 def proxyCheck(p):
     try:
         with session as s:
-            r = s.get(url, proxies={'https' :p, 'http':p}, timeout=1)
+            r = s.get(url, proxies={'https' :p, 'http':p}, timeout=5)
             if r.status_code == 200:
                 return p
             else:
@@ -35,11 +35,13 @@ def proxyCheck(p):
 
 #scrape proxies
 def get():
+
     proxies = []
     urls = ['https://www.us-proxy.org/','https://incloak.com/proxy-list/', 'http://free-proxy-list.net/anonymous-proxy.html', 'http://free-proxy-list.net/']
     print("scraping proxies")
     for u in urls:
-        for r in bs(session.get(u).text, 'html.parser').find_all('tr'):
+        try:
+            for r in bs(session.get(u, timeout=5).text, 'html.parser').find_all('tr'):
                 for i in r.contents[0]:
                     break
                 for p in r.contents[1]:
@@ -52,8 +54,17 @@ def get():
                         else:
                             if l not in proxies:
                                 proxies.append(l)
+        except:
+            print('connection to {0} failed'.format(u))
+        try:
+            r = requests.get("https://hidester.com/proxydata/php/data.php?mykey=data&offset=0&limit=100&orderBy=latest_check&sortOrder=DESC&country=&port=&type=undefined&anonymity=undefined&ping=undefined&gproxy=2", headers=headers)
+            l = json.loads(r.content)
+            for x in l:
+                if x['type'] == 'http':
+                    proxies.append(':'.join(x['IP'], x['PORT']))
+        except:
+            print('connection to {0} failed'.format('https://hidester.com/'))
     print("found {0}".format(len(proxies)))
     return proxies
-
 if __name__ == '__main__':
     about()
